331abf9945703e98d76c23ce59e29e4fee4977c8
gracezarczynska-sky <malgorzata.zarczynska@sky.uk>
2019-09-16 20:58:30 +0100

    redoing stuff

---
+++ component---src-pages-success-js-cd57b372bde20a4e9509.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{303:function(e,n,a){"use strict";a.r(n);var l=a(0),s=a.n(l);n.default=function(){return s.a.createElement("div",null,s.a.createElement("h1",null,"Success"),s.a.createElement("p",null,"Thank you."),s.a.createElement("p",null,"I will answer to your message as soon as possible."))}}}]);
+//# sourceMappingURL=component---src-pages-success-js-cd57b372bde20a4e9509.js.map
